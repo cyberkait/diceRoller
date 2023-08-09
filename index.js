5d635c7a05a6ac6f8e5861aac4f545beed59f54e
cyberkait <kaitlenius@gmail.com>
2023-08-09 17:08:05 -0600

    Numbers now display when the die is rolled.

--- index.js
+++ index.js
@@ -15,19 +15,19 @@ http.listen(port, () => console.log("Listening on port" + port));
 
 function onConnection(socket) {
     console.log("A user has connected!");
+
+
+    // Creating the dice roller.
+    function getNum() {
+        let n = Math.floor(Math.random() * 6);
+        return n + 1;
+    } //end function.
+
+
+    socket.on("roll", function () {
+        console.log("The roll button has been clicked.");
+        let diceValue = getNum();
+        socket.emit("finnish", diceValue);
+    })
 } // End function.
-
-
-// Creating the dice roller.
-function getNum() {
-    let n = Math.floor(Math.random() * 6);
-    return n + 1;
-} //end function.
-
-
-socket.on("roll", function () {
-    console.log("The roll button has been clicked.");
-    let diceValue = getNum();
-    socket.emit("finnish", diceValue);
-})
 
